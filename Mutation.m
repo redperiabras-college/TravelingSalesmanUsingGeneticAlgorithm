d3d098a2f3848a3b925d60866fdf1928a84ecfe2
redperiabras <redperiabras@gmail.com>
2017-02-18 01:10:29 +0800

    Final Code Documentation

--- Mutation.m
+++ Mutation.m
@@ -1,14 +1,36 @@
-function [ child ] = Mutation( child )
-%Muatation Summary of this function goes here
-%   Detailed explanation goes here
-    dna_size = length(child);
-    
-    point_1 = randi([1 dna_size], 1);
-    point_2 = randi([1 dna_size], 1);
+%*************************************************************************%
+%@BeginVerbatim 
+% Function: Mutation
+% Description: Modify the given chromosome
+% Parameter(s): chromosome, matrix
+% Filename: Mutation.m
+% Version: v00.02
+% Author: Group 1
+% Yr&Sec: 3-4
+% Subject: Computational Intelligence
+%@EndVebartim 
+%*************************************************************************%
 
-    temp = child(point_1);
-    child(point_1) = child(point_2);
-    child(point_2) = temp;
+% Last Reviewed by: Periabras, 17 February 2017
+
+%@Code Body***************************************************************%
+
+function [ child ] = Mutation( child )
+    %@Intialization*******************************************************%
+    DNA_SIZE = length(child);
+    %*********************************************************************%
+    
+    %Get 2 dna random points from the chromosome
+    points = randperm(DNA_SIZE, 2);
+
+    %Switch the 2 dna
+    temp = child(points(1));
+    child(points(1)) = child(points(2));
+    child(points(2)) = temp;
 
 end
+
+%*************************************************************************%
+
+%*************************************************************************%
 
